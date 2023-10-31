1edb33508b30e244f434896740115e57c7276706
siyamulislam <siyamulislam1@gmail.com>
2023-10-31 18:07:13 +0600

    single or multiple

--- app.js
+++ app.js
@@ -1,42 +1,71 @@
 const express = require('express');
+const multer = require('multer');
 const app = express();
-const multer = require("multer");
-const path = require("path");
+const port = 4000;
 
-// storage engine 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('uploads'));
 
-const storage = multer.diskStorage({
-    destination: './upload/files',
-    filename: (req, file, cb) => { 
-        // return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
-        return cb(null, `${file.fieldname}_${Date.now()}_${file.originalname}`)
-    }
-})
 
-const upload = multer({
-    storage: storage,
-    limits: {
-        fileSize: 10000000 //10 MB max
-    }
-})
-app.use('/file', express.static('upload/files'));
-app.post("/upload", upload.single('file'), (req, res) => {
-
-    res.json({
-        success: 1,
-        profile_url: `http://localhost:4000/file/${req.file.filename}`
-    })
-})
-
-function errHandler(err, req, res, next) {
+// Define the custom error handling middleware
+function handleUploadError(err, req, res, next) {
     if (err instanceof multer.MulterError) {
-        res.json({
-            success: 0,
-            message: err.message
-        })
+      res.status(400).json({ success: 0, message: err.message });
+    } else if (err) {
+      res.status(500).json({ success: 0, message: 'An unknown error occurred' });
+    } else {
+      next(); // Pass control to the next middleware if there are no errors
     }
-}
-app.use(errHandler);
-app.listen(4000, () => {
-    console.log("server running ~ hit at "+'http://localhost:4000/upload/');
-})
+  }
+  
+  // Attach the error handling middleware
+  app.use(handleUploadError);
+
+
+// Define storage with a custom filename function for singleUpload
+const singleUpload = multer({
+  dest: 'uploads',
+  limits: { fileSize: 10000000 },
+  storage: multer.diskStorage({
+    destination: 'uploads/profile', // Only specify the relative path here
+    filename: function (req, file, cb) {
+      // Customize the filename here
+      const uniqueSuffix = Date.now() + '-' +  Math.floor(100 + Math.random() * 900);
+      cb(null, uniqueSuffix +'-'+ file.originalname);
+    },
+  }),
+}).single('file');
+
+// Define storage with a custom filename function for multipleUpload
+const multipleUpload = multer({
+  dest: 'uploads',
+  storage: multer.diskStorage({
+    destination: 'uploads/products', // Only specify the relative path here
+    filename: function (req, file, cb) {
+      // Customize the filename here
+      const uniqueSuffix = Date.now() + '-' +  Math.floor(100 + Math.random() * 900);
+      cb(null, uniqueSuffix +'-'+ file.originalname);
+    },
+  }),
+}).array('files', 3);
+
+// For single file upload
+app.post('/upload', singleUpload, (req, res) => {
+  if (!req.file) {
+    return res.status(400).send('No file uploaded.');
+  }
+  res.send('File uploaded!');
+});
+
+// For multiple file upload
+app.post('/upload-multiple', multipleUpload, (req, res) => {
+  if (!req.files || req.files.length === 0) {
+    return res.status(400).send('No files uploaded.');
+  }
+  res.send(`${req.files.length} file(s) uploaded!`);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
