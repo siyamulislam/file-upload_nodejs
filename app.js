943a8db0c43408f3466e9677d15fb33ef08397b0
siyamulislam <siyamulislam1@gmail.com>
2023-10-31 23:59:03 +0600

    multer single and multiple file uploded

--- app.js
+++ app.js
@@ -7,65 +7,71 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('uploads'));
 
-
-// Define the custom error handling middleware
-function handleUploadError(err, req, res, next) {
-    if (err instanceof multer.MulterError) {
-      res.status(400).json({ success: 0, message: err.message });
-    } else if (err) {
-      res.status(500).json({ success: 0, message: 'An unknown error occurred' });
-    } else {
-      next(); // Pass control to the next middleware if there are no errors
-    }
-  }
-  
-  // Attach the error handling middleware
-  app.use(handleUploadError);
-
-
-// Define storage with a custom filename function for singleUpload
-const singleUpload = multer({
-  dest: 'uploads',
-  limits: { fileSize: 10000000 },
-  storage: multer.diskStorage({
-    destination: 'uploads/profile', // Only specify the relative path here
-    filename: function (req, file, cb) {
-      // Customize the filename here
-      const uniqueSuffix = Date.now() + '-' +  Math.floor(100 + Math.random() * 900);
-      cb(null, uniqueSuffix +'-'+ file.originalname);
-    },
-  }),
-}).single('file');
-
-// Define storage with a custom filename function for multipleUpload
-const multipleUpload = multer({
-  dest: 'uploads',
-  storage: multer.diskStorage({
-    destination: 'uploads/products', // Only specify the relative path here
-    filename: function (req, file, cb) {
-      // Customize the filename here
-      const uniqueSuffix = Date.now() + '-' +  Math.floor(100 + Math.random() * 900);
-      cb(null, uniqueSuffix +'-'+ file.originalname);
-    },
-  }),
-}).array('files', 3);
+const uploadOneMiddleware = (req, res, next) => {
+    const singleUpload = multer({
+        dest: 'uploads',
+        limits: { fileSize: 10000000 },
+        storage: multer.diskStorage({
+            destination: 'uploads/profile',
+            filename: function (req, file, cb) {
+                const uniqueSuffix = Date.now() + '-' + Math.floor(100 + Math.random() * 900);
+                cb(null, uniqueSuffix + '-' + file.originalname);
+            },
+        }),
+    }).single('file');
+    singleUpload(req, res, function (err) {
+        if (err instanceof multer.MulterError) {
+            err.status = 400
+            next(err)
+        } else if (err) {
+            const err = new Error('Server Error')
+            next(err)
+        }
+        next()
+    })
+}
+const uploadManyMiddleware = (req, res, next) => {
+    const multipleUpload = multer({
+        dest: 'uploads',
+        storage: multer.diskStorage({
+            destination: 'uploads/products',
+            filename: function (req, file, cb) {
+                const uniqueSuffix = Date.now() + '-' + Math.floor(100 + Math.random() * 900);
+                cb(null, uniqueSuffix + '-' + file.originalname);
+            },
+        }),
+    }).array('files', 3);
+    multipleUpload(req, res, function (err) {
+        if (err instanceof multer.MulterError) {
+            err.status = 400
+            next(err)
+        } else if (err) {
+            const err = new Error('Server Error')
+            next(err)
+        }
+        next()
+    })
+}
 
 // For single file upload
-app.post('/upload', singleUpload, (req, res) => {
-  if (!req.file) {
-    return res.status(400).send('No file uploaded.');
-  }
-  res.send('File uploaded!');
+app.post('/upload', uploadOneMiddleware, (req, res) => {
+    if (!req.file)
+        return res.status(400).send('No file uploaded.');
+    res.send('File uploaded!');
 });
 
 // For multiple file upload
-app.post('/upload-multiple', multipleUpload, (req, res) => {
-  if (!req.files || req.files.length === 0) {
-    return res.status(400).send('No files uploaded.');
-  }
-  res.send(`${req.files.length} file(s) uploaded!`);
+app.post('/upload-multiple', uploadManyMiddleware, (req, res) => {
+    if (!req.files || req.files.length === 0)
+        return res.status(400).send('No files uploaded.');
+    res.send(`${req.files.length} file(s) uploaded!`);
+});
+
+// Error-handling middleware
+app.use((err, req, res, next) => {
+    res.status(err.status ? err.status : 500).json({ error: err });
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
